6d47f81f63e201adcebbe79403a4bf01f996a226
Sakthi Vikkraman R <sakthi900145@gmail.com>
2026-02-22 20:44:35 +0530

    Git compose build

--- app/core/security.py
+++ app/core/security.py
@@ -1,9 +1,15 @@
 from passlib.context import CryptContext
+import hashlib
+import bcrypt
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str):
-    return pwd_context.hash(password)
+    password_bytes = password.encode('utf-8')
+    prehashed = hashlib.sha256(password_bytes).digest()
+    hashed = bcrypt.hashpw(prehashed, bcrypt.gensalt())
+    return hashed.decode('utf-8')
 
 def verify_password(plain_password: str, hashed_password: str):
-    return pwd_context.verify(plain_password, hashed_password)  
+    prehashed = hashlib.sha256(plain_password.encode('utf-8')).digest()
+    return bcrypt.checkpw(prehashed, hashed_password.encode('utf-8'))
